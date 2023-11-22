a312aff29e3a9fb1bfe1c4560d080028004d284a
Fabian Rauch <mail@rauch-fabian.de>
2023-11-22 16:09:32 +0100

    231122_after-course

--- sensorService/index.js
+++ sensorService/index.js
@@ -2,11 +2,22 @@ require('dotenv').config();
 const mqtt = require('mqtt');
 const sqlite3 = require('sqlite3').verbose();
 
+
+// DATABASE STUFF:
+
 let db = new sqlite3.Database('./mqtt_data.db', (err) => {
     if (err) {
         console.error(err.message);
     }
     console.log('Verbunden mit der SQLite-Datenbank.');
+
+    // Lösche alle Einträge beim Start
+    db.run('DELETE FROM temperature_data', (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('Alle vorherigen Einträge gelöscht.');
+    });
 });
 
 db.serialize(() => {
@@ -17,6 +28,8 @@ db.serialize(() => {
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
   )`);
 });
+
+// MQTT STUFF:
 
 const mqttClient = mqtt.connect(process.env.MQTT_BROKER_URL);
 
@@ -37,13 +50,18 @@ mqttClient.on('message', (topic, message) => {
     if (topic === 'dp2/temperature') {
         try {
             const data = JSON.parse(message.toString());
-            const stmt = db.prepare('INSERT INTO temperature_data (temperature, mac) VALUES (?, ?)');
-            stmt.run(data.temperature, data.mac);
-            stmt.finalize();
-            console.log(`Daten gespeichert: ${message.toString()}`);
+
+            // Überprüfe, ob die Werte NULL sind
+            if (data.temperature != null && data.mac != null) {
+                const stmt = db.prepare('INSERT INTO temperature_data (temperature, mac) VALUES (?, ?)');
+                stmt.run(data.temperature, data.mac);
+                stmt.finalize();
+                console.log(`Daten gespeichert: ${message.toString()}`);
+            } else {
+                console.log('NULL-Werte erkannt, Datensatz wird ignoriert.');
+            }
         } catch (e) {
             console.error(`Fehler beim Parsen der Nachricht: ${e}`);
         }
     }
 });
-
